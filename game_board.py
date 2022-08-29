015e2332297951185dd5aa983b76b84a74e1a296
Burak Ayaz <burakayaz@LAPTOP-Q03DMU24.localdomain>
2022-08-29 10:58:37 +0200

    seperate board vision and strategy into seperate classes

---
+++ game_board.py
@@ -0,0 +1,107 @@
+import cv2
+import pyautogui
+import numpy as np
+from random import *
+from enum import Enum
+import os
+
+
+class GameBoard:
+    def __init__(self):
+        screenshot = self.get_screenshot()
+        tiles = self.template_match(square, screenshot)
+        if not tiles:
+            print ('Minesweeper screen should be visible..')
+            exit(1)
+        x_diff = tiles[1][0] - tiles[0][0]
+        self.width = int((tiles[-1][0] - tiles[0][0]) / x_diff) + 1
+        y_diff = tiles[self.width][1] - tiles[0][1]
+        self.height = int((tiles[-1][1] - tiles[0][1]) / y_diff) + 1
+        self.tiles = np.reshape(tiles, (self.height, self.width, 2))
+        self.board = np.zeros((self.height, self.width), dtype=np.byte)
+        self.mouse = GameBoard.MouseController(self)
+        print('width: ' + str(self.width))
+        print('height: ' + str(self.height))
+
+    def template_match(self, small_image: np.ndarray, large_image: np.ndarray):
+        w, h = small_image.shape[:-1]
+        result = cv2.matchTemplate(small_image, large_image, cv2.TM_CCOEFF_NORMED)
+        threshold = .95
+        loc = np.where(result >= threshold)
+        results = []
+        for pt in zip(*loc[::-1]):  # Switch collumns and rows
+            results.append((int(pt[0] + w/2), int(pt[1] + h/2)))
+            #cv2.rectangle(large_image, pt, (pt[0] + w, pt[1] + h), (0, 0, 255), 2)
+        #cv2.imshow('res', large_image)
+        return results
+
+    def get_screenshot(self) -> np.ndarray:
+        image = pyautogui.screenshot()
+        return cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR)
+
+    class TileType(Enum):
+        UNKNOWN = 0
+        MINE = -1
+        CLEAR = 8
+        ONE = 1
+        TWO = 2
+        THREE = 3
+        FOUR = 4
+        FIVE = 5
+
+    class MouseController:
+        def __init__(self, outer_instance):
+            self.game = outer_instance
+            self.tiles = outer_instance.tiles
+
+        def click(self, x: int, y: int):
+            point = self.tiles[x][y]
+            pyautogui.click(point[0], point[1])
+
+        def move_away_mouse(self):
+            pyautogui.moveTo(self.tiles[0][0][0] - 50, self.tiles[0][0][1] - 50)
+
+    def get_surrounding_tiles(self, x, y):
+        surr = [(x+1,y),(x+1,y+1),(x+1,y-1),(x,y+1),
+                (x,y-1),(x-1,y-1),(x-1,y),(x-1,y+1)]
+        return [p for p in surr if p[0] >= 0 and p[0] < self.width
+                and p[1] >= 0 and p[1] < self.height]
+
+
+    def distance(self, p1, p2):
+        return abs(p1[0] - p2[0]) + abs(p1[1] - p2[1])
+
+    def update_tile(self, coord: (int, int), value: int):
+        for h in range(self.height):
+            for w in range(self.width):
+                if self.distance(self.tiles[h][w], coord) < 5:
+                    self.board[h][w] = value
+                    return
+
+    def find_figures(self, screenshot, image, value):
+        results = self.template_match(image, screenshot)
+        for p in results:
+            self.update_tile(p, value)
+        return results
+        
+
+    def update_board(self):
+        screenshot = self.get_screenshot()
+        if self.template_match(dead, screenshot):
+            exit(0)
+        self.find_figures(screenshot, one, 1)
+        self.find_figures(screenshot, two, 2)
+        self.find_figures(screenshot, three, 3)
+        self.find_figures(screenshot, four, 4)
+        self.find_figures(screenshot, five, 5)
+        self.board[self.board == 0] = 8
+        self.find_figures(screenshot, square, 0)
+
+os.chdir('images')
+square = cv2.imread('square.png')
+one = cv2.imread('one.png')
+two = cv2.imread('two.png')
+three = cv2.imread('three.png')
+four = cv2.imread('four.png')
+five = cv2.imread('five.png')
+dead = cv2.imread('dead.png')
--- main.py
+++ main.py
@@ -2,116 +2,68 @@ import cv2
 import pyautogui
 import numpy as np
 from random import *
+from enum import Enum
+from game_board import GameBoard
 
 
-def get_screenshot() -> np.ndarray:
-    image = pyautogui.screenshot()
-    return cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR)
-
-def template_match(small_image: np.ndarray, large_image: np.ndarray):
-    w, h = small_image.shape[:-1]
-    result = cv2.matchTemplate(small_image, large_image, cv2.TM_CCOEFF_NORMED)
-    threshold = .95
-    loc = np.where(result >= threshold)
-    results = []
-    for pt in zip(*loc[::-1]):  # Switch collumns and rows
-        results.append((int(pt[0] + w/2), int(pt[1] + h/2)))
-        #cv2.rectangle(large_image, pt, (pt[0] + w, pt[1] + h), (0, 0, 255), 2)
-    #cv2.imshow('res', large_image)
-    return results
-
-class Game:
+class Strategy(GameBoard):
     def __init__(self):
-        screenshot = get_screenshot()
-        tiles = template_match(square, screenshot)
-        if not tiles:
-            print ('Minesweeper screen should be visible..')
-            exit(1)
-        x_diff = tiles[1][0] - tiles[0][0]
-        self.width = int((tiles[-1][0] - tiles[0][0]) / x_diff) + 1
-        y_diff = tiles[self.width][1] - tiles[0][1]
-        self.height = int((tiles[-1][1] - tiles[0][1]) / y_diff) + 1
-        self.tiles = np.reshape(tiles, (self.height, self.width, 2))
-        # 0: unknown, 1,..6: mine numbers, 8:empty, -1: mine (flagged)
-        self.board = np.zeros((self.height, self.width), dtype=np.byte)
+        GameBoard.__init__(self)
         self.game()
 
-    def click(self, point):
-        pyautogui.click(point[0], point[1])
-
-    def distance(self, p1, p2):
-        return abs(p1[0] - p2[0]) + abs(p1[1] - p2[1])
-
-    def update_tile(self, coord: (int, int), value: int):
-        for w in range(self.width):
-            for h in range(self.height):
-                if self.distance(self.tiles[w][h], coord) < 5:
-                    self.board[w][h] = value
-                    return
-
-    def find_figures(self, screenshot, image, value):
-        results = template_match(image, screenshot)
-        for p in results:
-            self.update_tile(p, value)
-        return results
-        
-
-    def update_board(self):
-        screenshot = get_screenshot()
-        if self.find_figures(screenshot, dead, 0):
-            exit(0)
-        self.find_figures(screenshot, one, 1)
-        self.find_figures(screenshot, two, 2)
-        self.find_figures(screenshot, three, 3)
-        self.find_figures(screenshot, four, 4)
-        self.find_figures(screenshot, five, 5)           
-        for w in range(self.width):
-            for h in range(self.height):
-                if self.board[w][h] == 0:
-                    self.board[w][h] = 8
-        self.find_figures(screenshot, square, 0)
-
-
-    def move_away_mouse(self):
-        pyautogui.moveTo(self.tiles[0][0][0] - 50, self.tiles[0][0][1] - 50)
+    def filter_value(self, tiles, value):
+        res = []
+        for p in tiles:
+            x = p[0]
+            y = p[1]
+            if self.board[x][y] == value:
+                res.append(p)
+        return res
 
     def calculate_moves(self):
-        pass
+        moves = []
+        for h in range(self.height):
+            for w in range(self.width):
+                if self.board[h][w] == 1:
+                    surr = self.get_surrounding_tiles(w,h)
+                    
+                    ones = self.filter_value(surr, 1)
+                    if len(ones) == 1:
+                        moves.append(ones[0])
+                        
+                    
 
     def click_unknown_tile(self):
         for w in range(self.width):
             for h in range(self.height):
-                if self.board[w][h] == 0:
-                    self.click(self.tiles[w][h])
+                if self.board[h][w] == 0:
+                    self.mouse.click(w, h)
                     return
 
+    def flag_mines(self):
+        pass
+
+
     def game(self):
-        self.click(self.tiles[0][0]) #focus click
-        self.click(self.tiles[0][0]) #initial click
+        self.mouse.click(0, 0) #focus click
+        self.mouse.click(0, 0) #initial click
         
         while True:
-            self.move_away_mouse()
+            self.mouse.move_away_mouse()
             self.update_board()
-            print(self.board)
+            self.flag_mines()
+            
             moves = self.calculate_moves()
+            print(self.board)
             
             if not moves:
                 self.click_unknown_tile()
             else:
-                for tile in moves:
-                    self.click(tile)
+                for move in moves:
+                    self.click(move[0], move[1])
 
-square = cv2.imread('images/square.png')
-one = cv2.imread('images/one.png')
-two = cv2.imread('images/two.png')
-three = cv2.imread('images/three.png')
-four = cv2.imread('images/four.png')
-five = cv2.imread('images/five.png')
-empty = cv2.imread('images/empty.png')
-dead = cv2.imread('images/dead.png')
 
-Game()
-print("end")
+Strategy()
 
 
 
