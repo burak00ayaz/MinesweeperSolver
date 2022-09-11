3b0ac11e03d79fde7ee46027b9fea73e5f8a90b6
Burak Ayaz <burak.ayaz@kobil.com>
2022-09-11 22:58:54 +0200

    tiles and values generator

--- main.py
+++ main.py
@@ -12,44 +12,39 @@ class Strategy(GameBoard):
     def __init__(self):
         GameBoard.__init__(self)
 
-    def click_unknown_tile(self):
-        for h in range(self.height):
-            for w in range(self.width):
-                if self.board[h][w] == self.UNKNOWN:
-                    self.mouse.click(w, h)
-                    return
+    def tiles_with_values(self):
+        for y in range(self.height):
+            for x in range(self.width):
+                value = self.board[y][x]
+                yield (x,y), value
 
     def flag_mines(self):
         # We check surrounding tiles of a tile. If the sum of surrounding unknown tiles
         # and flagged mines equals the tile's number, we conclude that the unknown tiles are mines. 
-        for y in range(self.height):
-            for x in range(self.width):
-                value = self.board[y][x]
-                if value in [self.UNKNOWN, self.MINE, self.CLEAR]:
-                    continue
-                surr = self.get_surrounding_tiles(x,y)
-                unknowns = [p for p in surr if self.board[p[1]][p[0]] == self.UNKNOWN] #count unknowns
-                mines = [p for p in surr if self.board[p[1]][p[0]] == self.MINE] #count mines
-                if len(unknowns) + len(mines) == value:
-                    for u in unknowns:
-                        self.mouse.right_click(u[0], u[1])
-                        self.board[u[1]][u[0]] = self.MINE
+        for (x,y), value in self.tiles_with_values():
+            if value in [self.UNKNOWN, self.MINE, self.CLEAR]:
+                continue
+            surr = self.get_surrounding_tiles(x,y)
+            unknowns = [p for p in surr if self.board[p[1]][p[0]] == self.UNKNOWN] #count unknowns
+            mines = [p for p in surr if self.board[p[1]][p[0]] == self.MINE] #count mines
+            if len(unknowns) + len(mines) == value:
+                for u in unknowns:
+                    self.mouse.right_click(u[0], u[1])
+                    self.board[u[1]][u[0]] = self.MINE
 
     def possible_moves(self):
         # We check surrounding tiles of a tile. If the number of surrounding mines equals tile's number,
         # we conclude that surrounding unknown tiles are clear.
         moves = []
-        for y in range(self.height):
-            for x in range(self.width):
-                value = self.board[y][x]
-                if value in [self.UNKNOWN, self.MINE, self.CLEAR]:
-                    continue
-                surr = self.get_surrounding_tiles(x,y)
-                mines = [p for p in surr if self.board[p[1]][p[0]] == self.MINE] #count mines
-                if len(mines) == value:
-                    for p in surr:
-                        if self.board[p[1]][p[0]] == self.UNKNOWN: #every surrounding unknown is playable
-                            moves.append(p)
+        for (x,y), value in self.tiles_with_values():
+            if value in [self.UNKNOWN, self.MINE, self.CLEAR]:
+                continue
+            surr = self.get_surrounding_tiles(x,y)
+            mines = [p for p in surr if self.board[p[1]][p[0]] == self.MINE] #count mines
+            if len(mines) == value:
+                for p in surr:
+                    if self.board[p[1]][p[0]] == self.UNKNOWN: #every surrounding unknown is playable
+                        moves.append(p)
         return moves
 
     #TODO
@@ -65,14 +60,6 @@ class Strategy(GameBoard):
 
     def moves_with_chance(self, chance: float):
         pass
-        for y in range(self.height):
-            for x in range(self.width):
-                value = self.board[y][x]
-                if value in [self.UNKNOWN, self.MINE, self.CLEAR]:
-                    continue
-                surr = self.get_surrounding_tiles(x,y)
-                mines = [p for p in surr if self.board[p[1]][p[0]] == self.MINE] #count mines
-
 
     def game(self):
         self.mouse.click(0, 0) #focus click
@@ -88,7 +75,7 @@ class Strategy(GameBoard):
                 self.mouse.click(move[0], move[1])      
                         
             if not moves:
-                self.moves_with_chance()
+                self.moves_with_chance(0.5)
                 print('[+] I could not find a move.')
                 i = input('[+] Play a move and press Enter to let me continue.')
                 if i == "b":
